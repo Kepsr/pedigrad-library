c70c76aedb10cbf05ac090c959ad086091266ee8
James Hooker <jameshooker1999@live.com>
2022-06-12 23:09:58 +0100

    Refactoring inside PartitionCategory module

--- Pedigrad/PartitionCategory/cop.py
+++ Pedigrad/PartitionCategory/cop.py
@@ -11,12 +11,14 @@ from .cl_er import EquivalenceRelation
 
 def coproduct_of_partitions(partition1,partition2):
   #The following line checks if the coproduct is possible.
-  if len(partition1) == len(partition2):
-    #Returns the coproduct of two partitions as the quotient of the
-    #equivalence relation induced by the join of the preimages
-    #of the two partitions.
-    the_join = EquivalenceRelation(_join_preimages_of_partitions(_preimage_of_partition(partition1),_preimage_of_partition(partition2),FAST))
-    return the_join.quotient()
-  else:
-    print("Error: in coproduct_of_partitions: lengths do not match.")
-    exit()
+  assert len(partition1) == len(partition2), "lengths must match"
+
+  #Returns the coproduct of two partitions as the quotient of the
+  #equivalence relation induced by the join of the preimages
+  #of the two partitions.
+  join = EquivalenceRelation(_join_preimages_of_partitions(
+    _preimage_of_partition(partition1),
+    _preimage_of_partition(partition2),
+    FAST
+  ))
+  return join.quotient()
--- Pedigrad/PartitionCategory/efp.py
+++ Pedigrad/PartitionCategory/efp.py
@@ -1,27 +1,29 @@
-#------------------------------------------------------------------------------
-#_epi_factorize_partition(partition): list
-#------------------------------------------------------------------------------
-'''
-This function relabels the elements of a list with non-negative integers. It starts with the integer 0 and allocates a new label by increasing the previously allocated label by 1. The first element of the list always receives the label 0 and the highest integer used in the relabeling equals the length of the 'image' of the list decreased by 1 (see iop.py).
+from .iop import _image_of_partition
 
-e.g. _epi_factorize_partition(['A',4,'C','C','a','A']) = [0, 1, 2, 2, 3, 0]
+def _epi_factorize_partition(partition: list) -> list:
+  '''
+  Relabel the elements of a list with natural numbers.
 
-'''
-from Pedigrad.PartitionCategory.iop import _image_of_partition
+  Starting at 0, allocates a new label by increasing the previously allocated label by 1.
+  The first element of the list always receives the label 0
+  and the highest integer used in the relabeling
+  equals the length of the 'image' of the list decreased by 1 (see iop.py).
 
-def _epi_factorize_partition(partition):
+  e.g. _epi_factorize_partition(['A',4,'C','C','a','A']) = [0, 1, 2, 2, 3, 0]
+
+  '''
   #The relabeling depends on the cardinal of the image of the partition.
   #Computing the cardinal of the image is roughly the same as computing
   #the image itself.
-  the_image = _image_of_partition(partition)
+  image = _image_of_partition(partition)
   #A space is allocated to contain the relabeled list.
-  epimorphism=list()
+  epimorphism = []
   #If the i-th element of the list is the j-th element of the image
   #then this element is relabelled by the integer j.
-  for i in range(len(partition)):
-    for j in range(len(the_image)):
-      if partition[i] == the_image[j]:
-        epimorphism.append(j)
+  for x in partition:
+    for i, y in enumerate(image):
+      if x == y:
+        epimorphism.append(i)
         break
   #Returns the relabeled list.
   return epimorphism
