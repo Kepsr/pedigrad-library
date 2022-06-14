a54ec17c044dcb80f7bdbea5517c2f4ca161b676
James Hooker <jameshooker1999@live.com>
2022-06-14 08:02:39 +0100

    Simplified PointedSet

--- Pedigrad/AlignedFunctor/cl_pst.py
+++ Pedigrad/AlignedFunctor/cl_pst.py
@@ -1,52 +1,19 @@
-#------------------------------------------------------------------------------
-#PointedSet (Class) | 2 objects | 2 methods
-#------------------------------------------------------------------------------
-'''
-[Objects]
-  .symbols  [Type] list('a)
-  .index    [Type] int
 
-[Methods] 
-  .__init__
-        [Inputs: 2]
-          - symbols [Type] list('a)
-          - index [Type] int
-        [Outputs: 0]
-  .point
-        [Inputs: 0]
-        [Outputs: 1]
-          - return [Type] 'a
-           
-[General description] 
-  This structure models the features of a pointed set. It is made of an object [symbols] containing a list of the elements of the underlying set, and an object [index] giving the index at which the point of the pointed set is located whithin [symbols].
-    
->>> Method: .__init__
-  [Actions] 
-    .symbols  <- use(symbol)
-    .index <- use(index)
-  [Description] 
-    This is the constructor of the class.
 
->>> Method: .point
-  [Actions] 
-    .return  <- use(self.symbol,self.index)
-  [Description] 
-    Returns the symbol of the point of the pointed structure.
-    
-'''
-#------------------------------------------------------------------------------
-#CODE
-#------------------------------------------------------------------------------
-class PointedSet: 
-#------------------------------------------------------------------------------
-  def __init__(self, symbols, index: int):
-    if index < 0 or index >= len(symbols):
-      self.index = 0
-      self.symbols = ['*'] + symbols
-    else:
-      self.index = index
-      self.symbols = symbols
-#------------------------------------------------------------------------------
+class PointedSet:
+  '''
+  This class models a pointed set (also called a based set or rooted set),
+  an ordered pair (X, x0) of a set X and an element x0 in X called the base point.
+  A `PointedSet` stores a list `symbols` of the elements of the set,
+  and an index `index` into `symbols` where the base point is located.
+  '''
+
+  def __init__(self, symbols: list, index: int):
+    assert 0 <= index < len(symbols)
+    self.symbols = symbols
+    self.index = index
+
   def point(self):
+    ''' Return the base point.
+    '''
     return self.symbols[self.index]
-#------------------------------------------------------------------------------
