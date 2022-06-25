2a28aec9125cf09d84402845d0a118ca6af0f809
James Hooker <jameshooker1999@live.com>
2022-06-25 10:49:11 +0100

    Refactored _preimage_of_partition for performance
    Inlined _epi_factorize_partition(partition),
    so that nub(partition) wouldn't be called twice.

--- Pedigrad/PartitionCategory/piop.py
+++ Pedigrad/PartitionCategory/piop.py
@@ -1,31 +1,44 @@
-from .efp import _epi_factorize_partition
 from Pedigrad.utils import nub
 
 def _preimage_of_partition(partition: list) -> list[list]:
   '''
-  This function takes a list and returns the list of the lists of indices that index the same element.
+  Given a list, return a list of lists of indices that index the same element.
 
   e.g. _preimage_of_partition(['a','a',2,2,3,3,'a']) = [[0,1,6],[2,3],[4,5]]
 
-  From the point of view of partitions, the returned list is the preimage of the epimorphism associated with the partition, where the preimage is defined as the indexed set of the fibers of the epimorphism.
+  From the point of view of partitions,
+  the returned list is the preimage of the epimorphism associated with the partition,
+  where the preimage is defined as the indexed set of the fibers of the epimorphism.
 
   The previous example:
-  - the list [0,1,6] is the fiber of the element 'a';
-  - the list [2,3] is the fiber of the element 2;
-  - the list [4,5] is the fiber of the element 3.
+  - the list [0, 1, 6] is the fiber of the element 'a';
+  - the list [2, 3] is the fiber of the element 2;
+  - the list [4, 5] is the fiber of the element 3.
 
-  The preimage will always order its fibers with respect to the order in which the elements of the input list appear.
-
+  The order of fibers in `preimage` will preserve (modulo duplicates)
+  the order of elements in `partition`.
   '''
+  image = nub(partition)
   # preimage will store the fibers of the partition.
-  preimage = [[] for _ in nub(partition)]
-  # The number of fibers contained by the preimage is equal
-  # to the number of elements in the image of the partition.
+  # For each element in the image, there will be a fiber in the preimage.
+  preimage = [[] for _ in image]
   # The relabeled list of 'partition' gives the desired indexing
-  # of the fibers contained in the preimage of partition.
-  epimorphism = _epi_factorize_partition(partition)
+  # of the fibers in the preimage.
+  epimorphism = (image.index(x) for x in partition)
+  # Fill the fibers
   for i, j in enumerate(epimorphism):
-    # Append i to the j-th fiber of the preimage.
+    # Append i to the j-th fiber of the preimage
     preimage[j].append(i)
-  # After the loop, all the fibers are filled and the preimage is returned
   return preimage
+  # NOTE This function is equivalent to:
+  # [[i for i, y in enumerate(partition) if y == x] for x in nub(partition)]
+  # but in practice appears to be quite a bit faster,
+  # probably because it doesn't have any nested loops.
+
+
+def __test():
+  xs = ['a', 'a', 2, 2, 3, 3, 'a']
+  assert _preimage_of_partition(xs) == [[0, 1, 6], [2, 3], [4, 5]]
+
+
+__test()
