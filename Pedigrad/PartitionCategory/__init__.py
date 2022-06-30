25ae8fc056a801b87c91e24cb79d433d8a5e50fa
James Hooker <jameshooker1999@live.com>
2022-06-30 22:10:08 +0100

    Moved product_of_partitions and coproduct_of_partitions together
    Also provided alternative implementations.
    The alternative implementation of coproduct is broken,
    since it will sometimes separate tuples too eagerly.

--- Pedigrad/PartitionCategory/__init__.py
+++ Pedigrad/PartitionCategory/__init__.py
@@ -2,6 +2,5 @@ from .efp import _epi_factorize_partition
 from .piop import partition_from_list
 from .jpop import join_partitions, FAST
 from .cl_er import Partition
-from .cop import coproduct_of_partitions
-from .pop import product_of_partitions
+from .product import product_of_partitions, coproduct_of_partitions
 from .cl_mop import MorphismOfPartitions
--- Pedigrad/PartitionCategory/efp.py
+++ Pedigrad/PartitionCategory/efp.py
@@ -1,19 +1,19 @@
 from Pedigrad.utils import nub
 
 
-def _epi_factorize_partition(partition: list) -> list:
+def _epi_factorize_partition(xs: list) -> list:
   '''
   Relabel the elements of a list with indices.
 
-  Let `image` be `partition` with no duplicates.
-  Then, this function will return a "factorization" of `partition`
-  in which each element of `partition` is replaced by its index in `image`.
+  Let `image` be `xs` with no duplicates.
+  Then, this function will return a "factorization" of `xs`
+  in which each element of `xs` is replaced by its index in `image`.
   The new labels will thus come from `range(len(image))`.
   '''
-  # The relabeling depends on the cardinal of the image of the partition.
+  # The relabeling depends on the cardinal of the image of xs.
   # The cardinal of the image is roughly the same as the image itself.
-  image = nub(partition)
-  return [image.index(x) for x in partition]
+  image = nub(xs)
+  return [image.index(x) for x in xs]
 
 
 # An epimorphism is a morphism f: a -> b that is right-cancellative:
@@ -28,7 +28,7 @@ def _epi_factorize_partition(partition: list) -> list:
 
 
 def __test():
-  assert _epi_factorize_partition('A4CCaA') == [0, 1, 2, 2, 3, 0]
+  assert _epi_factorize_partition('abccda') == [0, 1, 2, 2, 3, 0]
   assert _epi_factorize_partition('abc') == _epi_factorize_partition('123')  # [0, 1, 2]
 
 
